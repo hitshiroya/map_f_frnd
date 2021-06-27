80addf02ffceca9d1dc46414a1481b44b4526109
Hit@Shiroya <hitp363530@gmail.com>
2021-06-27 15:02:50 +0530

    done

---
+++ store.js
@@ -0,0 +1,75 @@
+const storeList = [
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ 71.2028993,22.5784479]
+        },
+        "properties": {
+          "name": "Hit Shiroya",
+          "address": "Fulwadi plot near school number-7 Thangadh363530.",
+          "phone": "+91 9510823023"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [70.6585656, 20.8174707]
+        },
+        "properties": {
+          "name": "Samarth Kariya",
+          "address": "RadhaSwami Park Society Near Railway Crossing Kodinar 362720.",
+          "phone": "+91 85447 55650"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [70.7865993,22.2574231]
+        },
+        "properties": {
+          "name": "Sohan Panchasara",
+          "address": "Asharay Greens behind Fortune Hotel Rajkot36005.",
+          "phone": "+91 99043 40461"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [70.7975413,22.2784729]
+        },
+        "properties": {
+          "name": "Pal Pujara",
+          "address": "Ahiya Apt Behind Swaminarayan gurukul gondal road Rajkot 36005",
+          "phone": "+91 74350 30163"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [70.8238317,22.7986733]
+        },
+        "properties": {
+          "name": "Anjali Manek",
+          "address": "Ravapar road Morbi363641",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [70.7865993,22.2574231]
+        },
+        "properties": {
+          "name": "Khyati Bhensdadia",
+          "address": "Umiya Chowk Rajkot",
+          "phone": "23 2323 2323"
+        }
+      }
+      
+]
